63e15823ea88be9fad9b989b0190b78fb52cccfc
Esteban Gonzalez <esteban@estebandev.xyz>
2025-12-04 18:57:54 -0500

    feat: add miscellaneous options

---
+++ extra.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func GitBranch() string {
+	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+
+	content, _ := cmd.Output()
+
+	return strings.Trim(string(content), "\n")
+}
+
+func GitStatus_NoClean(data []any) string {
+
+	cmd := exec.Command("git", "status", "--porcelain")
+
+	content, _ := cmd.Output()
+
+	output := ""
+
+	if len(content) > 1 {
+		output = Parser(Lexer(data))
+	}
+
+	return output
+}
+
+func GitStatus_Clean(data []any) string {
+
+	cmd := exec.Command("git", "status", "--porcelain")
+
+	content, _ := cmd.Output()
+
+	output := ""
+
+	if len(content) < 1 {
+		output = Parser(Lexer(data))
+	}
+
+	return output
+}
+
+func Execute(cmd string) string {
+	cmdParts := strings.Split(cmd, " ")
+	command := exec.Command(cmdParts[0], cmdParts[1:]...)
+	output, _ := command.Output()
+	return strings.Trim(string(output), "\n")
+}
--- main.go
+++ main.go
@@ -12,8 +12,8 @@ type Type int
 
 const (
 	Env Type = iota
-	Str   
-	Color 
+	Str
+	Color
 )
 
 type Token struct {
@@ -25,43 +25,77 @@ var m map[string]Token
 
 func initMap() {
 	dir, _ := os.Getwd()
-	dir = strings.ReplaceAll(dir,os.Getenv("HOME"), "~")
+	dir = strings.ReplaceAll(dir, os.Getenv("HOME"), "~")
 
 	hostname, _ := os.ReadFile("/etc/hostname")
 
 	m = make(map[string]Token)
 	m["dir"] = Token{T: Str, V: dir}
 	m["hostname"] = Token{T: Str, V: string(hostname)[:len(hostname)-1]}
+	m["git_branch"] = Token{T: Str, V: GitBranch()}
 }
 
-func Lexer(data []byte) []Token {
-	tokens := []Token{}
-
-	var conf []string
+func Converter(data []byte) []any {
+	var conf []any
 	err := json.Unmarshal(data, &conf)
 
 	if err != nil {
 		log.Fatal("malformed config file ", err)
 	}
 
-	for _, v := range conf {
+	return conf
+}
+
+func Lexer(conf []any) []Token {
+
+	tokens := []Token{}
+
+	for _, confVal := range conf {
 		token := Token{}
-		if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
-			var ok bool
-			token, ok = m[v[2:len(v)-1]]
-			if !ok {
+
+		switch val := confVal.(type) {
+		case string:
+			if strings.HasPrefix(val, "${") && strings.HasSuffix(val, "}") {
+				var ok bool
+				token, ok = m[val[2:len(val)-1]]
+				if !ok {
+					token.T = Str
+					token.V = val
+				}
+			} else if strings.HasPrefix(val, "$") {
+				token.T = Env
+				token.V = val[1:]
+			} else if strings.HasPrefix(val, "bg:") || strings.HasPrefix(val, "fg:") || strings.HasPrefix(val, "c:") {
+				token.T = Color
+				token.V = val
+			} else if strings.HasPrefix(val, "exec:") {
 				token.T = Str
-				token.V = v
+				token.V = Execute(val[5:])
+			} else {
+				token.T = Str
+				token.V = val
 			}
-		} else if strings.HasPrefix(v, "$") {
-			token.T = Env
-			token.V = v[1:]
-		} else if strings.HasPrefix(v, "bg:") || strings.HasPrefix(v, "fg:") || strings.HasPrefix(v, "c:") {
-			token.T = Color
-			token.V = v
-		} else {
-			token.T = Str
-			token.V = v
+		case map[string]any:
+			for k, v := range val {
+				switch k {
+				case "git_status_noclean":
+					if arr, ok := v.([]any); ok {
+						content := GitStatus_NoClean(arr)
+						if content != "" {
+							token.T = Str
+							token.V = content
+						}
+					}
+				case "git_status_clean":
+					if arr, ok := v.([]any); ok {
+						content := GitStatus_Clean(arr)
+						if content != "" {
+							token.T = Str
+							token.V = content
+						}
+					}
+				}
+			}
 		}
 
 		tokens = append(tokens, token)
@@ -89,7 +123,7 @@ func Parser(tokens []Token) string {
 
 func main() {
 
-	data, err := os.ReadFile("config.json")
+	content, err := os.ReadFile("~/.config/")
 
 	if err != nil {
 		log.Fatal("config file missing", err)
@@ -97,5 +131,6 @@ func main() {
 	initMap()
 	initColorMap()
 
+	data := Converter(content)
 	fmt.Println(Parser(Lexer(data)))
 }
